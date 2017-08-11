b5048282fdbd08cbd00c0c2d411d204cc67a0ad2
parveennannemia <30703851+parveennannemia@users.noreply.github.com>
2017-08-11 16:42:51 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,11 +14,11 @@ res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/article-two',function(req,res){
-   res.send('Article two reuest is served'); 
+   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
 app.get('/article-three',function(req,res){
-   res.send('Article three reuest is served'); 
+   res.sendFile(path.join(__dirname, 'ui', 'article-three.html')); 
 });
 
 app.get('/ui/style.css', function (req, res) {
