b33e11e2e36ea6d40d3d0ad92e2b66ac79136144
parveennannemia <30703851+parveennannemia@users.noreply.github.com>
2017-08-11 20:17:43 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -28,7 +28,8 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function(req,res){
-res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+//res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.send(createTemplate(article1));
 });
 
 app.get('/article-two',function(req,res){
