92fbe61d7c6312e03e7b6b13f923ee55296ba3e0
parveennannemia <30703851+parveennannemia@users.noreply.github.com>
2017-08-18 10:28:29 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -40,6 +40,7 @@ app.get('/', function (req, res) {
 var pool = new Pool(config);
 app.get('/test-db',function(req,res){
    pool.query ('SELECT * FROM test',function (err,result){
+       alert(err);
        alert(JSON.stringfy(pool));
        if(err){
            res.statusCode = 500;
