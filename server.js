938ffc35023d1214e95dc5723a8d08ae01fc5235
parveennannemia <30703851+parveennannemia@users.noreply.github.com>
2017-08-18 14:28:02 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -66,8 +66,8 @@ app.get('/articles/:articleName',function(req,res){
         console.info("result" + result);
         if(err){
            //res.statusCode = 500;
-            //return res.send('Error 500: No quote found' );
-          return res.status(500).send(err.toString());
+            return res.send('Error 500: No quote found' );
+         // return res.status(500).send(err.toString());
        }else
        if(result.rows.length()=== 0){
            //res.statusCode = 404;
