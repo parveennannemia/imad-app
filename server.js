145dd82b31260e61158808027787c238915b54c1
parveennannemia <30703851+parveennannemia@users.noreply.github.com>
2017-08-18 14:00:38 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -62,9 +62,9 @@ app.get('/articles/:articleName',function(req,res){
     pool.query("select * from article where title= " + articleName,function (err,result){
         if(err){
            res.statusCode = 500;
-            //return res.send(err.toString());
-          return res.status(500).send(err.toString());
-       }else if(result.rows.length()== 0){
+            return res.send('Error 404: No quote found' );
+          //return res.status(500).send(err.toString());
+       }else if(result.rows.length()=== 0){
            res.statusCode = 404;
             return res.send('Error 404: Article Not found');
        }
@@ -74,6 +74,11 @@ app.get('/articles/:articleName',function(req,res){
            return  res.send(createTemplate(articleData));
        }
        } );
+       
+
+  //var q = quotes[req.params.id];
+  //res.json(q);
+//});
    
 });
 
