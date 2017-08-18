240377a84e928e2dcf79759c302fce01ed15cb4b
parveennannemia <30703851+parveennannemia@users.noreply.github.com>
2017-08-18 10:27:11 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.get('/', function (req, res) {
 var pool = new Pool(config);
 app.get('/test-db',function(req,res){
    pool.query ('SELECT * FROM test',function (err,result){
-       alert(JSON.stringfy(result));
+       alert(JSON.stringfy(pool));
        if(err){
            res.statusCode = 500;
             return res.send('Error 500: No records found');
